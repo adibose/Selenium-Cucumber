14656875a80acee5b120e2770e6c7023a78cc803
shila <shila@LAPTOP-90969VRN>
2022-08-04 23:49:10 +0530

    Added Dependency Injection

---
+++ Selenium-CucumberAutomationFramework/src/main/java/context/TestContext.java
@@ -0,0 +1,13 @@
+package context;
+
+import org.openqa.selenium.WebDriver;
+
+import domainObjects.PersonalDetails;
+
+public class TestContext {
+	
+	public WebDriver driver;
+	public PersonalDetails pDetails;
+	public String userName;
+
+}
