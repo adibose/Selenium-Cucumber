0b39e34dc8ee0a4805bd7347ba227570b671cccd
shila <shila@LAPTOP-90969VRN>
2022-08-04 09:50:04 +0530

    Updated Home Page

--- Selenium-CucumberAutomationFramework/src/main/java/pages/HomePage.java
+++ Selenium-CucumberAutomationFramework/src/main/java/pages/HomePage.java
@@ -1,5 +1,12 @@
 package pages;
 
-public class HomePage {
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends BasePage {
+
+	public HomePage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
 
 }
