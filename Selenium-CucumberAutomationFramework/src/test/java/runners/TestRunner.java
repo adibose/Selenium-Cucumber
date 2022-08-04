a4de1e3a18223e775834c7b11b161523dabd50b9
shila <shila@LAPTOP-90969VRN>
2022-08-04 21:08:35 +0530

    Updated Test Runner file to include custom data table type and update plugin option

---
+++ Selenium-CucumberAutomationFramework/src/test/java/runners/TestRunner.java
@@ -0,0 +1,17 @@
+package runners;
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber; 
+
+@RunWith(Cucumber.class)
+@CucumberOptions
+(features="src/test/resources/Features",
+glue={"StepDefinitions","hooks","customType"},
+monochrome = true,
+plugin = {"pretty", "html:target/CucumberReports/cucumber.html"},
+tags = "@SIT"
+)
+public class TestRunner {
+
+}
